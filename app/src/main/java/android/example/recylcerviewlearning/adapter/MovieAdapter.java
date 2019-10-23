72d7f612854f7719aa321087154ebbfb3e569622
Grant McDonnal <grantmcdonnal@gmail.com>
2019-10-23 11:26:48 -0500

    Intents, Detail-Activity, YouTube video player

--- app/src/main/java/android/example/recylcerviewlearning/adapter/MovieAdapter.java
+++ app/src/main/java/android/example/recylcerviewlearning/adapter/MovieAdapter.java
@@ -1,7 +1,9 @@
 package android.example.recylcerviewlearning.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.example.recylcerviewlearning.DetailActivity;
 import android.example.recylcerviewlearning.R;
 import android.example.recylcerviewlearning.models.Movie;
 import android.provider.ContactsContract;
@@ -9,13 +11,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
+import org.parceler.Parcels;
 import org.w3c.dom.Text;
 
 import java.util.List;
@@ -28,35 +33,6 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     public MovieAdapter(List<Movie> movies, Context context){
         this.movieList = movies;
         this.context = context;
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView posterView;
-        TextView titleView;
-        TextView descriptionView;
-
-        public ViewHolder(View itemView){
-            super(itemView);
-
-            posterView = itemView.findViewById(R.id.imageView2);
-            titleView = itemView.findViewById(R.id.textView);
-            descriptionView = itemView.findViewById(R.id.textView2);
-        }
-
-        public void bind(Movie movie) {
-            titleView.setText(movie.getTitle());
-            descriptionView.setText(movie.getDescription());
-
-            String imageUrl;
-            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-                imageUrl = movie.getBackdropPath();
-            }else{
-                imageUrl = movie.getPosterUrl();
-            }
-            Glide.with(context)
-                .load(imageUrl)
-                .into(posterView);
-        }
     }
 
     @NonNull
@@ -80,5 +56,47 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     public int getItemCount() {
         return movieList.size();
     }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout container;
+        ImageView posterView;
+        TextView titleView;
+        TextView descriptionView;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+
+            posterView = itemView.findViewById(R.id.imageView2);
+            titleView = itemView.findViewById(R.id.textView);
+            descriptionView = itemView.findViewById(R.id.textView2);
+            container = itemView.findViewById(R.id.container);
+        }
+
+        public void bind(final Movie movie) {
+            titleView.setText(movie.getTitle());
+            descriptionView.setText(movie.getDescription());
+
+            String imageUrl;
+            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                imageUrl = movie.getBackdropPath();
+            }else{
+                imageUrl = movie.getPosterUrl();
+            }
+            Glide.with(context)
+                .load(imageUrl)
+                .into(posterView);
+
+            container.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
+        }
+    }
+
+
 
 }
--- app/src/main/java/android/example/recylcerviewlearning/models/Movie.java
+++ app/src/main/java/android/example/recylcerviewlearning/models/Movie.java
@@ -3,21 +3,32 @@ package android.example.recylcerviewlearning.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
     private String backdropPath;
     private String posterUrl;
     private String title;
     private String description;
+    double rating;
+    int movieId;
+
+    // empty constructor needed by Parceler library
+    public Movie(){
+
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterUrl = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         description = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public String getPosterUrl() {
@@ -34,6 +45,14 @@ public class Movie {
 
     public String getBackdropPath(){
         return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public double getRating(){
+        return rating;
+    }
+
+    public int getMovieId(){
+        return movieId;
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
